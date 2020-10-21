bef52e87e4fb90f0eeb1b821051ceb05dbbbeb33
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-21 23:32:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,8 +8,8 @@ function forLoop(array) {
   for(let i = 0; i < 25; i++) {
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's' }.""`)
     console.log(array[i])
-    return array;
   }
+  return array;
 }
 
 function whileLoop(n) {
